d45b9870869fc0c650c8391f259c607cefc5ec61
FEDERICO MAURO AGUIRRE <RS40365119@urg.grupoosde>
2026-04-09 12:53:42 -0300

    Implement GBFS

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -3,6 +3,8 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
+def heuristic(state, end):
+    return abs(state[0]-end[0]) + abs(state[1]- end[1])
 
 class GreedyBestFirstSearch:
     @staticmethod
@@ -23,7 +25,37 @@ class GreedyBestFirstSearch:
         reached[root.state] = root.cost
 
         # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontier = PriorityQueueFrontier()
+        frontier.add(root, priority=heuristic(root.state, grid.end))
+
+        while True:
+            #Check empty
+            if frontier.is_empty():
+                return NoSolution(reached)
+            
+            #Remove next node from frontier
+            node= frontier.pop()
+
+            #Expand node
+            for action in grid.actions(node.state):
+
+                #Get the successor
+                successor = grid.result(node.state, action)
+
+                #Check if the succesor was already reached
+                if successor in reached:
+                    continue
+
+                #Initialize the son node
+                son = Node("",successor,cost=node.cost + grid.individual_cost(node.state, action), parent= node, action=action)
+                #Mark the successor as reached
+                reached[successor]= True
+
+                #Aplly objetive test
+                if grid.objective_test(successor):
+                    return Solution(son, reached)
+                
+                #Add son to frontier
+                frontier.add(son, priority=heuristic(successor, grid.end))
 
         return NoSolution(reached)
